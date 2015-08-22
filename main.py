5283a2f6c444a362b4a09c76e0921058f3e90395
Chris Stevens <contact@cstevens.biz>
2015-08-22 11:02:43 +0100

    Improved the handling of rate limiting - is now non blocking when waiting to tweet again.

--- main.py
+++ main.py
@@ -5,16 +5,18 @@ import secure as s
 auth = tweepy.OAuthHandler(s.CONSUMER_KEY, s.CONSUMER_SECRET)
 auth.set_access_token(s.OAUTH_KEY, s.OAUTH_SECRET)
 api = tweepy.API(auth)
+tweet_at = 0
 
 
 class Listener(tweepy.StreamListener):
     def on_status(self, status):
-        if "RT" not in status.text:
+        global tweet_at
+        if "RT" not in status.text and time.time() > tweet_at:
             try:
                 new = api.update_status(status=u"\U0001F692" + " No fire tweets please @" + status.author.screen_name,
                                         in_reply_to_status_id=status.id)
                 print("https://twitter.com/NoFireTweetsPls/status/" + str(new.id))
-                time.sleep(100)
+                tweet_at = time.time() + 100
             except tweepy.error.TweepError as e:
                 print(e.response)
 
